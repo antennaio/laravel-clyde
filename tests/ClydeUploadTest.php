dc5de144e59fb9e6df78348dc170aaa22f096379
kazik <antenna@antenna.io>
2016-04-01 16:57:46 +0200

    Mocking Illuminate\Config\Repository

--- tests/ClydeUploadTest.php
+++ tests/ClydeUploadTest.php
@@ -4,6 +4,7 @@ namespace Antennaio\Clyde\Test;
 
 use Antennaio\Clyde\ClydeUpload;
 use Antennaio\Clyde\FilenameGenerator;
+use Illuminate\Config\Repository as Config;
 use Illuminate\Contracts\Filesystem\Factory as Filesystem;
 use Illuminate\Contracts\Filesystem\FilesystemAdapter;
 use Mockery;
@@ -19,10 +20,13 @@ class ClydeUploadTest extends TestCase
         $this->disk = Mockery::mock(FilesystemAdapter::class);
         $this->filename = Mockery::mock(FilenameGenerator::class);
         $this->uploadedFile = Mockery::mock(UploadedFile::class);
+        $this->config = Mockery::mock(Config::class);
 
         $this->files->shouldReceive('disk')->once()->andReturn($this->disk);
+        $this->config->shouldReceive('get')->with('clyde.source')->once()->andReturn('local');
+        $this->config->shouldReceive('get')->with('clyde.source_path_prefix')->once()->andReturn('/uploads');
 
-        $this->uploads = new ClydeUpload($this->files, $this->filename);
+        $this->uploads = new ClydeUpload($this->config, $this->files, $this->filename);
     }
 
     public function testUpload()
