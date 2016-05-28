c739b5c67c3bc24e859a1ec210073c16f64815f0
kazik <antenna@antenna.io>
2016-05-28 09:58:17 +0200

    Route parameter could contain a slash

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -3,4 +3,4 @@
 Route::get('/'.config('clyde.url_prefix').'/{path}', [
     'as' => config('clyde.route_name'),
     'uses' => 'Antennaio\Clyde\Http\Controllers\ClydeImageController@show',
-]);
+])->where('path', '(.*)');
