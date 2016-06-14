0f3465db6bcb2cfa3a1927bc083df55901eaf37c
kazik <antenna@antenna.io>
2016-06-14 17:11:25 +0200

    Adding Server facade

---
+++ src/Facades/Server.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Antennaio\Clyde\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class Server extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return 'laravel-clyde-server';
+    }
+}
