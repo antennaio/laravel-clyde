efbd890a3a0a9825496af39b560406429f40a0c3
kazik <antenna@antenna.io>
2016-06-14 17:12:52 +0200

    Making use of Server facade in ClydeImageController

--- src/Http/Controllers/ClydeImageController.php
+++ src/Http/Controllers/ClydeImageController.php
@@ -2,29 +2,14 @@
 
 namespace Antennaio\Clyde\Http\Controllers;
 
-use Illuminate\Contracts\Filesystem\Factory as Filesystem;
+use Antennaio\Clyde\Facades\Server;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
-use League\Glide\Responses\SymfonyResponseFactory;
-use League\Glide\ServerFactory;
 use League\Glide\Signatures\SignatureFactory;
 use League\Glide\Signatures\SignatureException;
 
 class ClydeImageController extends Controller
 {
-    /**
-     * @var Filesystem
-     */
-    protected $files;
-
-    /**
-     * @param Filesystem $files
-     */
-    public function __construct(Filesystem $files)
-    {
-        $this->files = $files;
-    }
-
     /**
      * Display image.
      *
@@ -44,19 +29,6 @@ class ClydeImageController extends Controller
             }
         }
 
-        $server = ServerFactory::create([
-            'source' => $this->files->disk(config('clyde.source'))->getDriver(),
-            'source_path_prefix' => config('clyde.source_path_prefix'),
-            'cache' => $this->files->disk(config('clyde.cache'))->getDriver(),
-            'cache_path_prefix' => config('clyde.cache_path_prefix'),
-            'watermarks' => $this->files->disk(config('clyde.watermarks'))->getDriver(),
-            'watermarks_path_prefix' => config('clyde.watermarks_path_prefix'),
-            'driver' => config('clyde.driver'),
-            'max_image_size' => config('clyde.max_image_size'),
-            'presets' => config('clyde.presets'),
-            'response' => new SymfonyResponseFactory(),
-        ]);
-
-        return $server->outputImage($filename, $request->all());
+        return Server::outputImage($filename, $request->all());
     }
 }
