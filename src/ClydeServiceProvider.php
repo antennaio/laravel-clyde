816f95d472a70b04f637d47fb56dd7e1038b8b6a
kazik <antenna@antenna.io>
2016-06-14 17:10:59 +0200

    Binding Server in ClydeServiceProvider

--- src/ClydeServiceProvider.php
+++ src/ClydeServiceProvider.php
@@ -2,7 +2,11 @@
 
 namespace Antennaio\Clyde;
 
+use Antennaio\Clyde\Contracts\Server;
+use Illuminate\Contracts\Filesystem\Factory as Filesystem;
 use Illuminate\Support\ServiceProvider;
+use League\Glide\Responses\SymfonyResponseFactory;
+use League\Glide\ServerFactory;
 
 class ClydeServiceProvider extends ServiceProvider
 {
@@ -26,6 +30,23 @@ class ClydeServiceProvider extends ServiceProvider
     public function register()
     {
         $this->mergeConfigFrom(__DIR__.'/../config/clyde.php', 'clyde');
+
+        $this->app->singleton('laravel-clyde-server', function ($app) {
+            $files = $app->make(Filesystem::class);
+
+            return ServerFactory::create([
+                'source' => $files->disk(config('clyde.source'))->getDriver(),
+                'source_path_prefix' => config('clyde.source_path_prefix'),
+                'cache' => $files->disk(config('clyde.cache'))->getDriver(),
+                'cache_path_prefix' => config('clyde.cache_path_prefix'),
+                'watermarks' => $files->disk(config('clyde.watermarks'))->getDriver(),
+                'watermarks_path_prefix' => config('clyde.watermarks_path_prefix'),
+                'driver' => config('clyde.driver'),
+                'max_image_size' => config('clyde.max_image_size'),
+                'presets' => config('clyde.presets'),
+                'response' => new SymfonyResponseFactory(),
+            ]);
+        });
 
         $this->app->bind('laravel-clyde-image', function ($app) {
             return $app->make(ClydeImage::class);
